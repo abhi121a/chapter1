958f84b1aa77e8048352b6cd38f95fb76da64567
abhi121a <abhi121a@gmail.com>
2014-07-24 01:36:02 +0530

    chapter1
    simple test program

---
+++ taskone.java
@@ -0,0 +1,12 @@
+package chapter1;
+
+public class taskone {
+
+	public static void main(String[] args) {
+	System.out.println("this class was created to test the GIT");
+	for(int i=10;i>=0;i--)
+		System.out.println("GIT works");
+
+	}
+
+}
